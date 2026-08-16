3a3c5381816b6a5b5a90642b6ee6126749d6eedf
bizmaky <bizmak@hotmail.co.uk>
2026-08-16 20:41:55 +0100

    FORCESFIT V3 - vintage branding and colours

---
+++ sw-v3.js
@@ -0,0 +1,34 @@
+const CACHE='forcesfit-v3';
+const CORE=['./','./index.html','./manifest.webmanifest','./forcesfit-192.png','./forcesfit-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
+});
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.method!=='GET') return;
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req).then(res=>{
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(req,copy));
+        return res;
+      }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then(cached=>cached||fetch(req).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy));
+      return res;
+    }))
+  );
+});
