a452b3161103cba5389792e354cbc299d7b30d2a
bizmaky <bizmak@hotmail.co.uk>
2026-08-17 11:28:15 +0100

    Add files via upload

--- sw-v4.js
+++ sw-v4.js
@@ -1,4 +1,4 @@
-const CACHE='forcesfit-v4';
+const CACHE='forcesfit-v4.1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./forcesfit-192.png','./forcesfit-512.png','./sw-v4.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
